c5556fab6c07be2633f773647af5feb525727f06
namoo-san <highgraphics.x@gmail.com>
2019-08-02 17:14:26 +0900

    Convert images to grayscale

---
+++ imgconvert.py
@@ -0,0 +1,17 @@
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
+import sys
+import glob
+import pyocr
+import pyocr.builders
+
+def convert ():
+    lists = glob.glob("./img/*", recursive=True)
+    for cv_img in lists:
+        print(cv_img)
+        filename = cv_img.replace('img', 'result')
+        print(filename)
+        img = Image.open(cv_img)
+        img.convert('L').save(filename),
+
+if __name__ == "__main__" :
+    convert()
